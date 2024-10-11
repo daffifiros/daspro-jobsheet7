c279830785f6ef78747df050bb388faa5ced3240
daffifirops <daffikun10@gmail.com>
2024-10-11 15:05:47 +0700

    Modifikasi

--- SiAkadWhile17.java
+++ SiAkadWhile17.java
@@ -1,3 +1,43 @@
+// import java.util.Scanner;
+// public class SiAkadWhile17 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int nilai, jml, i = 0;
+//         System.out.print("Masukkan jumlah mahasiswa: ");
+//         jml = sc.nextInt();
+//         while (i < jml) {
+//             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+//             nilai = sc.nextInt();
+            
+//             if (nilai < 0 || nilai > 100) {
+//                 System.out.println("Nilai tidak valid. Masukkan lagi nilai yang valid!");
+//                 continue;
+                
+//             }
+
+//             if (nilai > 80 && nilai <= 100) {
+//                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah A");
+//             } else if (nilai > 73 && nilai <= 80) {
+//                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B+");
+//             } else if (nilai > 65 && nilai <= 73) {
+//                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B");
+//             } else if (nilai > 60 && nilai <= 65) {
+//                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah C+");
+//             } else if (nilai > 50 && nilai <= 60) {
+//                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah C");
+//             } else if (nilai > 39 && nilai <= 50) {
+//                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah D");
+//             } else {
+//                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah E");
+//             }
+//             i++;
+            
+//         }
+
+//     }
+    
+// }
+
 import java.util.Scanner;
 public class SiAkadWhile17 {
     public static void main(String[] args) {
@@ -12,11 +52,11 @@ public class SiAkadWhile17 {
             if (nilai < 0 || nilai > 100) {
                 System.out.println("Nilai tidak valid. Masukkan lagi nilai yang valid!");
                 continue;
-                
             }
 
             if (nilai > 80 && nilai <= 100) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah A");
+                System.out.println("Bagus, pertahankan nilainya!");
             } else if (nilai > 73 && nilai <= 80) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B+");
             } else if (nilai > 65 && nilai <= 73) {
@@ -31,9 +71,6 @@ public class SiAkadWhile17 {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah E");
             }
             i++;
-            
         }
-
     }
-    
 }
